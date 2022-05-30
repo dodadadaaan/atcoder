f186724eebf315fce702d9621ce483fcb806db02
dodadadaaan <56538733+dodadadaaan@users.noreply.github.com>
2022-05-30 18:54:17 +0900

    update

--- test.py
+++ test.py
@@ -7,4 +7,21 @@ a4 = [[0 for i in range(10)] for j in range(3)] #空のリスト
 for i in range(n):
 	b, f, r, v = map(int,input().split())
 	if b == 1:
-		a1[1][] = 
+		a1[f-1][r-1] = v
+
+	elif b == 2:
+		a2[f-1][r-1] = v
+
+	elif b == 3:
+		a3[f-1][r-1] = v
+
+	else:
+		a4[f-1][r-1] = v
+
+print(a1)
+print('#'*10)
+print(a2)
+print('#'*10)
+print(a3)
+print('#'*10)
+print(a4)
