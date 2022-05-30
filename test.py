6f98160070ebc1d80ea3de94e8fd848d9a37db51
dodadadaaan <dodadadaaan.t@fuji.waseda.jp>
2022-05-30 12:37:22 +0900

    upload

--- test.py
+++ test.py
@@ -1,11 +1,25 @@
-class MyClass:
-    i = 12345
+n = int(input())
+a = list() #空のリスト
 
-    def f(self):
-        return 'Hello World'
-
-x = MyClass
-
-xf = x.f
-while True:
-    print(xf)
+for i in range(n):
+	x, y = input().split() #x:文字，y:数字の格納
+	y = int(y)
+    #文字の一致から数字を読み取り，空のリストに数字を格納する
+	if x == "S":
+		a.append(y + 0)
+	if x == "H":
+		a.append(y + 13)
+	if x == "C":
+		a.append(y + 26)
+	if x == "D":
+		a.append(y + 39)
+for i in range(1, 53):
+	if not (i in a):
+		if i <= 13:
+			print("S {}".format(i - 0))
+		elif i <= 26:
+			print("H {}".format(i - 13))
+		elif i <= 39:
+			print("C {}".format(i - 26))
+		else:
+			print("D {}".format(i - 39))
