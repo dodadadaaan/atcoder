7f43bce8c3c2806df96adb5ea743b3ab73072484
dodadadaaan <56538733+dodadadaaan@users.noreply.github.com>
2022-05-30 18:46:40 +0900

    updated

--- test.py
+++ test.py
@@ -5,7 +5,6 @@ a3 = [[0 for i in range(10)] for j in range(3)] #空のリスト
 a4 = [[0 for i in range(10)] for j in range(3)] #空のリスト
 
 for i in range(n):
-	if x == "C":
 	b, f, r, v = map(int,input().split())
 	if b == 1:
 		a1[1][] = 
